94be359ea72564678ed3d7c495a18d91940bf59b
Jonathan Buso <jonathanbuso.26@gmail.com>
2022-07-11 18:24:42 +0200

    404 page added link to previous page

--- frontend/src/pages/Error404.js
+++ frontend/src/pages/Error404.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
-import { Container } from "reactstrap";
+import { Button, Container } from "reactstrap";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 
 export default function Error404() {
+    const history = useHistory();
     return (
         <>
             <NavBar />
@@ -15,7 +16,12 @@ export default function Error404() {
                     <div className="rounded border-1 active">404</div>
                     <div className="centered">
                         Oops ! We could not find the page you were looking for.
-                        <Link to="/home">Return to homepage</Link>
+                        <button
+                            className="link"
+                            onClick={() => history.goBack()}
+                        >
+                            Return to previous page.
+                        </button>
                     </div>
                 </div>
             </Container>
